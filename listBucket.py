5fe893bfd0ef9f6b53e15674a2809f1b3a6d43d3
gurnamfs <gurunam.lohia@firstsource.com>
2024-11-20 13:16:33 +0530

    Changed how env is accessed

--- listBucket.py
+++ listBucket.py
@@ -12,27 +12,8 @@ import json
 
 load_dotenv()
 
-PROJECT_ID = os.getenv("PROJECT_ID")
-PRIVATE_KEY_ID = os.getenv("PRIVATE_KEY_ID")
-PRIVATE_KEY = os.getenv("PRIVATE_KEY")
-CLIENT_EMAIL = os.getenv("CLIENT_EMAIL")
-CLIENT_ID = os.getenv("CLIENT_ID")
-CLIENT_CERT = os.getenv("CLIENT_CERT")
-
-
-credentials_info = {
-  "type": "service_account",
-  "project_id": PROJECT_ID,
-  "private_key_id": PRIVATE_KEY_ID,
-  "private_key": PRIVATE_KEY,
-  "client_email": CLIENT_EMAIL,
-  "client_id": CLIENT_ID,
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": CLIENT_CERT,
-  "universe_domain": "googleapis.com"
-}
+credentials_info = json.loads(os.getenv("API_KEY"))
+# PROJECT_ID = credentials_info['PROJECT_ID']
 
 
 # with open('path_to_credentials.json', 'r') as file:
@@ -40,7 +21,8 @@ credentials_info = {
 
 def get_access_token():
     scopes = ["https://www.googleapis.com/auth/cloud-platform"]
-    quota_project_id = PROJECT_ID
+    # quota_project_id = PROJECT_ID
+    quota_project_id = "firstsource-vertex"
     credentials, project = load_credentials_from_dict(credentials_info, scopes=scopes, quota_project_id=quota_project_id)
     credentials.refresh(Request())
     return credentials.token, project
