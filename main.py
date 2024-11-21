49aa87379c435ea4a4d1a985dded7bc9d715df51
ChellaaFS <chellaam@firstsource.com>
2024-11-21 15:08:07 +0530

    removing the process text func

--- main.py
+++ main.py
@@ -2,18 +2,11 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import os
 import requests
-import vertexai
-from google.oauth2 import service_account
 import google.auth
 from google.auth.transport.requests import Request
-from typing import List
-import re
-from vertexai.generative_models import GenerativeModel
-from datetime import datetime
-from google.cloud import aiplatform
 
 app = FastAPI()
-from listBucket import gcp_create_bucket, list_buckets , upload_files_to_gcs , process_text
+from listBucket import gcp_create_bucket, list_buckets , upload_files_to_gcs
 
 class BucketRequest(BaseModel):
     bucket_name: str
@@ -24,8 +17,6 @@ class UploadFileRequest(BaseModel):
     object_names: List[str] 
     file_paths: List[str] 
 
-class InputText(BaseModel):
-    text: str
 
 
 @app.post("/create_bucket")
@@ -43,9 +34,3 @@ async def get_buckets():
 async def upload_file(request: UploadFileRequest):
     """Endpoint to upload files to a GCS bucket."""
     return upload_files_to_gcs(request.bucket_name, request.object_names, request.file_paths)
-
-@app.post("/process_text")
-async def process_text_endpoint(input: InputText):
-    """Endpoint to process the input text using Vertex AI."""
-    formatted_output = process_text(input.text)
-    return {"formatted_output": formatted_output}
